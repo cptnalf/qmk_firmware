b7973e569b47344655264ae9ad7eccbeaf1d9089
cptnalf <captainalf@gmail.com>
2020-07-08 17:53:40 -0400

    add rgblayers
    * rgb layer for:
      function 1
      function 2
      scroll-lock

--- keyboards/kbdfans/kbd75/keymaps/cptnalf/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/cptnalf/keymap.c
@@ -1,19 +1,35 @@
 #include QMK_KEYBOARD_H
 
-//#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+const rgblight_segment_t PROGMEM scrolllock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {7, 1, HSV_BLUE }
+);
 
-#define _______ KC_TRNS
-#define __xxxx_ KC_NO
+const rgblight_segment_t PROGMEM kbfx_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+  {0, 2, HSV_MAGENTA },
+  {14, 2, HSV_MAGENTA }
+);
+
+const rgblight_segment_t PROGMEM fxr_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+  {6, 2, HSV_CYAN },
+  {8, 2, HSV_CYAN }
+);
+
+const rgblight_segment_t* const PROGMEM cptnalf_rgblayers[] = RGBLIGHT_LAYERS_LIST(
+  scrolllock_layer,
+  kbfx_layer,
+  fxr_layer
+);
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_BRK,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   __xxxx_,  KC_BSPC,  KC_INS,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_NO,  KC_BSPC,  KC_INS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_DEL,
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_HOME,
     KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      __xxxx_,  KC_SPC,   __xxxx_,                      KC_RALT,LT(2,KC_APP), KC_RCTL,KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_NO,    KC_SPC,   KC_NO  ,                      KC_RALT,LT(2,KC_APP), KC_RCTL,KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
 	[1] = LAYOUT(
@@ -31,61 +47,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,            _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,
     _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,  _______,
-    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______, KC_PGDOWN, _______
+    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  KC_HOME, KC_PGDOWN, KC_END
   )
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-
-
-	switch (id) {
-
-	}
-	return MACRO_NONE;
+layer_state_t layer_state_set_user(layer_state_t state) {
+  rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+  rgblight_set_layer_state(2, layer_state_cmp(state, 2));
+  return state;
 }
 
-void matrix_init_user(void) {
+bool led_update_user(led_t led_state) {
+  rgblight_set_layer_state(0, led_state.scroll_lock);
+  return true;
 }
 
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		DDRB |= (1 << 2); PORTB &= ~(1 << 2);
-	} else {
-		DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-	//	DDRB |= (1 <<1); PORTB &= ~(1 << 1);
-	} else {
-	//	DDRB &= ~(1 << 1); PORTB &= ~(1 << 1);
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-
-	} else {
-
-	}
-
+void keyboard_post_init_user(void) {
+  rgblight_layers = cptnalf_rgblayers;
 }
 
 /*
@@ -123,4 +101,3 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   }
 }
 */
-
